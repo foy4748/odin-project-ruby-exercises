8fbceb9b344d65e408797b77c7d7f216c0f23681
foy4748 <faisaljfcl@gmail.com>
2023-03-15 00:12:51 +0600

    1_data_types string_exercises DONE

--- ruby_basics/1_data_types/exercises/string_exercises.rb
+++ ruby_basics/1_data_types/exercises/string_exercises.rb
@@ -1,40 +1,50 @@
 def concatenate_example(string)
   # use concatenation to format the result to be "Classic <string>"
-  "Classic " << string
+  #"Classic " << string
+  return "Classic " + string
 end
 
 def concatenate(string)
   # use concatenation to format the result to be "Hello <string>!"
+  return "Hello #{string}!"
 end
 
 def substrings(word)
   # return the first 4 letters from the word using substrings
+  return word[0,4]
 end
 
 def capitalize(word)
   # capitalize the first letter of the word
+  return word[0].uppercase
 end
 
 def uppercase(string)
   # uppercase all letters in the string
+  return word.uppercase
 end
 
 def downcase(string)
   # downcase all letters in the string
+  return string.lowercase
 end
 
-def empty_string(string)
+def empty_string?(string)
   # return true if the string is empty
+  return string.empty?
 end
 
 def string_length(string)
   # return the length of the string
+  string.length
 end
 
 def reverse(string)
   # return the same string, with all of its characters reversed
+  return string.reverse!
 end
 
 def space_remover(string)
   # remove all the spaces in the string using gsub
+  return space_remover.gsub(" ","")
 end
